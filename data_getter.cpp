feb8784c0bc03d8d853d39a3ac9f1c84a9c46446
Nikolay Nechaev <nikolay_nechaev@mail.ru>
2019-10-30 14:55:30 +0300

    Data getter: fixes and 3mes getter

--- data_getter.cpp
+++ data_getter.cpp
@@ -6,6 +6,15 @@
 #include <sstream>
 using namespace std;
 
+template<class T1, class T2, class T3>
+struct trio
+{
+	T1 first;
+	T2 second;
+	T3 third;
+};
+
+
 unsigned long long int get_n_of_lines(const string &file_name)
 {
 	ifstream f(file_name);
@@ -25,14 +34,18 @@ void skip_to_semicolon(istringstream &ss)
 	}
 }
 
-vector<pair<double, double>> get_credit_deposit_rate(const string &table_name) {
-	/*Returns a pair of the credit/deposit value and it's rate
+vector<pair<double, double>> get_credit_deposit_rate(const string &table_name)
+{
+	/* Returns a vector of pairs of the credit/deposit values and it's rates
 	 * Attention: credit value will be positive, deposit value - negative
 	 Parameters:
 	 	table_name - The csv file name to read data from. Attention:
 			credit value must be first column, deposit value must be
 			second column, rate must be fifth column, first line must
 			contain columns' headers
+	Returns:
+		A vector of pairs, where first is either credit or deposit value (different signs),
+			second is the rate
 	*/
 	ifstream f(table_name);
 	string tmp;
@@ -53,6 +66,45 @@ vector<pair<double, double>> get_credit_deposit_rate(const string &table_name) {
 		    ans.push_back({value.first, 0});
 		for(int i = 0; i < 3; ++i)
 			skip_to_semicolon(ss);
+		ss >> ans.back().second;
+	}
+	return ans;
+}
+
+vector<trio<double, double, int>> get_credit_deposit_rate_maturity(const string &table_name)
+{
+	/* Returns a vector of trios (first, second, third) of the credit/deposit values, it's rates
+	 * and it's maturity
+	 * Attention: credit value will be positive, deposit value - negative
+	Parameters:
+		table_name - The csv file name to read data from. Attention:
+			credit value must be first column, deposit value must be
+			second column, rate must be fifth column, maturity must be
+			forth command, first line must contain columns' headers
+	Returns:
+		A vector of trios, where first is either credit or deposit value (different signs),
+			second is the rate, third is the maturity
+	*/
+	ifstream f(table_name);
+	string tmp;
+	getline(f, tmp);
+
+	vector<trio<double, double, int>> ans;
+	ans.reserve(get_n_of_lines(table_name));
+	while(getline(f, tmp))
+	{
+		istringstream ss(tmp);
+		pair<double, double> value;
+		ss >> value.first;
+		skip_to_semicolon(ss);
+		ss >> value.second;
+		if(value.first == 0)
+			ans.push_back({-value.second, 0, 0});
+		else
+			ans.push_back({value.first, 0, 0});
+		skip_to_semicolon(ss); skip_to_semicolon(ss);
+		ss >> ans.back().third;
+		skip_to_semicolon(ss);
 		ss >> ans.back().second;
 	}
 	return ans;
